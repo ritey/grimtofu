dd852171dcf23d8aa3638c3b5fc711d7d7e4f39d
David Wright <dave@ritey.com>
2017-02-03 12:12:13 +0000

    Replace [question_mark] with ? on views

--- webroot/resources/views/pages/index.blade.php
+++ webroot/resources/views/pages/index.blade.php
@@ -8,7 +8,7 @@
         <div class="col-sm-10 offset-sm-1 item">
 
             <div class="col-sm-12">
-                <h4><a href="{{ route('thread', ['channel' => strtolower($item['labels'][0]['name']), 'title' => strtolower(str_replace(' ','-',$item['title'])) . '::'.$item['number']]) }}">{{ $item['title'] }}</a></h4>
+                <h4><a href="{{ route('thread', ['channel' => strtolower($item['labels'][0]['name']), 'title' => strtolower(str_replace(' ','-',$item['title'])) . '::'.$item['number']]) }}">{{ str_replace('[question_mark]','?',$item['title']) }}</a></h4>
                 <p><a href="{{ route('channel', ['channel' => strtolower($item['labels'][0]['name'])]) }}">{{ $item['labels'][0]['name'] }}</a> &middot; <a href="">{{ $item['user']['login'] }}</a> &middot; {{ $item['created_at'] }}</p>
                 <p>{{ $item['body'] }}</p>
             </div>
