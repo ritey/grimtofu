3e47299ed55ed663ea061aefc3ce4f4eb51aec42
David Wright <dave@ritey.com>
2017-02-04 14:00:07 +0000

    Add avatar

--- webroot/resources/views/pages/index.blade.php
+++ webroot/resources/views/pages/index.blade.php
@@ -9,14 +9,17 @@ All threads
 <div class="container">
     @foreach($vars['threads'] as $item)
     <div class="row">
-        <div class="col-sm-10 offset-sm-1 item">
+        <div class="col-sm-1 avatar">
+            <img src="{{ $item['avatar'] }}" alt="{{ $item['username'] }}" />
+        </div>
+        <div class="col-sm-10 item">
 
             <div class="col-sm-12">
                 <h4><a href="{{ route('thread', ['channel' => $item['label'], 'title' => $item['slug']]) }}">{{ $item['clean_title'] }}</a></h4>
                 <div class="alert-dismissible">
                     <div class="close">{{ $item['comments'] }}</div>
                 </div>
-                <p><a href="{{ route('channel', ['channel' => $item['label']]) }}">{{ $item['label'] }}</a> &middot; <a href="">{{ $item['username'] }}</a> &middot; {{ $item['created_at'] }}</p>
+                <p><a href="{{ route('channel', ['channel' => $item['label']]) }}">{{ $item['label'] }}</a> &middot; By <a href="">{{ $item['username'] }}</a> &middot; {{ $item['created_at'] }}</p>
                 <p>{{ $item['body'] }}</p>
             </div>
 
